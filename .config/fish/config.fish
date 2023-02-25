4ab389108016962964edb0a3ecdcae7732b1142d
Cedric Anthony <git@antho.dev>
2023-02-25 16:26:38 +0100

    🔧 config: updated gitconfig, fish and tmux configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,26 +17,37 @@ complete --command aws --no-files --arguments '(begin; set --local --export COMP
 
 alias gts='git status'
 alias gtp='git pull --rebase'
-alias gtr='git rebase'
 alias gtc='git commit -S -m'
+alias gta='git add'
 alias gco='git checkout'
 alias gfo='git fetch origin'
 alias gffs='git flow feature start'
 alias gfff='git flow feature finish'
-alias sag='sudo apt update'
-alias sagu='sudo apt upgrade'
-alias sai='sudo apt install'
+alias upd='sudo nala update'
+alias upg='sudo nala upgrade'
+alias inst='sudo nala install'
 alias nv='nvim'
-alias lg='lazygit'
 
 alias xl='exa -al --icons'
 
-alias up='docker-compose up -d'
-alias stop='docker-compose stop'
-alias down='docker-compose down'
+alias dcud='docker-compose up -d'
+alias dcs='docker-compose stop'
+alias dcd='docker-compose down'
+
+alias tekyn='cd ~/projects/tekyn/web-platform'
+alias ssh-tekyn='ssh tekynweb@51.91.247.113 -p 2311'
 
 alias t='tmux -2'
 alias ta='t attach -t'
 alias tn='t new -s'
+alias talw='ta tekyn_leg'
+alias tarw='ta -t tekyn_ref'
 
 alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+
+# starship init fish | source
+
+# Created by `pipx` on 2022-12-05 18:19:07
+set PATH $PATH /home/anthodev/.local/bin
+
+source ~/.agent-bridge_fish.sh
