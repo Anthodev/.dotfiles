ef9a75297e358daf4b45d95447d56fe73dc8a9c6
Cedric ANTHONY <git@antho.dev>
2024-03-27 23:52:57 +0100

    ✨ feat(fish): revamped fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,42 +6,37 @@ set -gx TERM "xterm-256color"
 set -gx LC_ALL "fr_FR.UTF-8"
 set -gx LANG "fr_FR.UTF-8"
 
-if not pgrep -f ssh-agent > /dev/null
-	eval (ssh-agent -c)
-	set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-	set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-	set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-end
-
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
-alias gts='git status'
+
+set -lx IS_WSL (eval uname -r)
+if string match -q -- '*WSL2' $IS_WSL
+    alias ssh='ssh.exe'
+    alias ssh-add='ssh-add.exe'
+end
+
+alias gs='git status'
+alias ga='git add .'
 alias gtp='git pull --rebase'
+alias gpf='git push --force'
 alias gtc='git commit -S -m'
 alias gta='git add'
 alias gco='git checkout'
 alias gfo='git fetch origin'
-alias gffs='git flow feature start'
-alias gfff='git flow feature finish'
-alias upd='sudo nala update'
-alias upg='sudo nala upgrade'
-alias inst='yay -S'
-alias rem='yay -Rns'
+alias upd='sudo paru'
+alias inst='paru -S'
+alias rem='paru -Rns'
 alias nv='nvim'
 
-alias xl='exa -al --icons'
+alias xl='eza -al --icons'
 
-alias dcud='docker-compose up -d'
-alias dcs='docker-compose stop'
-alias dcd='docker-compose down'
+alias dcud='docker compose up -d'
+alias dcs='docker compose stop'
+alias dcd='docker compose down'
 
 alias t='tmux -2'
 alias ta='t attach -t'
 alias tn='t new -s'
 
-alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
-
-### UNCOMMENT THIS PART TO ENABLE 1Password SSH-Agent ON WSL2
-# Created by `pipx` on 2022-12-05 18:19:07
-# set PATH $PATH /home/anthodev/.local/bin
-# source ~/.agent-bridge_fish.sh
+alias sail='[ -f sai
+                     l ] && sh sail || sh vendor/bin/sail'
